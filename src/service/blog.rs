e9dd354bf9d69ebe44f2a7a4f9a97492ff081295
zhangwt <im.zhangwt@gmail.com>
2023-09-27 14:08:30 +0800

    fix: 修复github api403错误

--- src/service/blog.rs
+++ src/service/blog.rs
@@ -1,32 +1,33 @@
-use reqwest::Error;
-use std::process::Command;
 use crate::models::api_req::GithubApiReq;
 use crate::models::api_resp::IssuesResponse;
+use reqwest::Error;
 use reqwest::StatusCode;
+use std::process::Command;
 
 // 请求github api
 #[tokio::main]
 pub async fn req_api(req: GithubApiReq) -> Result<(), Error> {
     let url = format!(
-        "https://api.github.com/repos/{}/{}/issues?state=all",
+        "https://api.github.com/repos/{}/{}/issues?staet=all",
         req.owner, req.repo
     );
     let resp = reqwest::Client::new()
         .get(&url)
+        .header("User-Agent", "blog-archive")
         .header("Accept", "application/vnd.github+json")
         .header("X-GitHub-Api-Version", "2022-11-28")
         .header("Authorization", format!("Bearer {}", req.token))
         .send()
         .await?;
 
-    match  resp.status() {
+    match resp.status() {
         StatusCode::OK => {
-            let issues_list: Vec<IssuesResponse> = resp.json().await?; 
+            let issues_list: Vec<IssuesResponse> = resp.json().await?;
             handle_issues(issues_list)
-        },
+        }
         StatusCode::UNAUTHORIZED => {
             println!("error: {}", resp.status());
-        },
+        }
         _ => {
             println!("error: {}", resp.status());
         }
@@ -38,7 +39,9 @@ pub async fn req_api(req: GithubApiReq) -> Result<(), Error> {
 fn handle_issues(issues_list: Vec<IssuesResponse>) {
     let mut text = String::new();
     for issue in issues_list {
-        text.push_str(format!("[{}]({}) - {}\n", issue.title, issue.url, issue.created_at).as_str());
+        text.push_str(
+            format!("[{}]({}) - {}\n", issue.title, issue.url, issue.created_at).as_str(),
+        );
     }
     update_readme(text);
 }
@@ -50,5 +53,6 @@ fn update_readme(text: String) {
         .arg(text)
         .arg(">>")
         .arg("README.md")
-        .output().expect("echo failed"); 
+        .output()
+        .expect("echo failed");
 }
